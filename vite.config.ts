18f385371a4869bede6b6580a51530b8e6fc2239
Takinur M <takinurm@gmail.com>
2026-04-17 00:14:09 +0100

    Skeleton

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,14 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
+import { crx } from '@crxjs/vite-plugin'
+import manifest from './manifest.json'
 
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(),
-    tailwindcss()
+  tailwindcss(),
+  crx({ manifest })
   ],
 })
